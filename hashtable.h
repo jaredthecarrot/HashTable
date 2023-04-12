a9f8c655810f8a8f9866f862ef324cc76c83e2f5
Jared McCain <jaredmccain.jam@gmail.com>
2023-04-12 12:30:19 -0500

    layout for separate chaining

--- hashtable.h
+++ hashtable.h
@@ -79,4 +79,42 @@ int hashtable_lp::erase(int k){
     }
 }
 
+class hashtable_sc{
+    private:
+    int table[N];
+    int n;
+    int collisions;
+    public:
+    int size();
+    hashtable_sc();
+    int h(int k);
+    // find
+    int find(int k);
+    // put
+    int put(int k);
+    // erase
+    int erase(int k);
+    // need linear probing
+    int collision();
+
+};
+
+hashtable_sc::hashtable_sc(){
+    for (int i = 0; i < N; i++)
+        table[i] = NULL;
+    n = 0;
+    collisions = 0;
+}
+
+int hashtable_sc::size(){return n;}
+
+int hashtable_sc::collision(){return collisions;}
+
+int hashtable_sc::h(int k){return k % N;}
+
+// implement find
+
+// put
+
+// erase
 #endif
